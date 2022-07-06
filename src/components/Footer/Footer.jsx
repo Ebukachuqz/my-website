633c90ef4471b77c53cd6afb4a196441f056185c
EbukaChuqz <ebukachuqz@gmail.com>
2022-07-06 20:35:29 +0100

    Built footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,34 @@
+import { styled } from "@mui/material";
 import React from "react";
 
+const Container = styled("footer")`
+  background: #1e1e1e;
+  min-height: 50px;
+  margin: 30px auto;
+  padding: 10px;
+  color: #fff;
+  border-radius: var(--border-radius);
+`;
+
+const FooterText = styled("p")`
+  color: #9c9c9c;
+  font-size: 12px;
+  text-align: center;
+  padding: 5px;
+  width: 100%;
+  span {
+    color: #fff;
+    font-size: 13px;
+  }
+`;
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Container>
+      <FooterText>
+        © 2022 All rights reserved. Created by <span>Ebuka Chuqz</span>
+      </FooterText>
+    </Container>
+  );
 };
 
 export default Footer;
